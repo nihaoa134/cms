9d362499e721529901826c2832d7730d1d6ff107
nihaoa134 <2337865364@qq.com>
2019-03-22 10:26:35 +0000

    123

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -143,11 +143,11 @@ class WxController extends Controller
 	}
     public function  wxlogincode(Request $request){
         print_r($_GET);
+        exit;
         $appid = "wx0ed775ffa80afa46";
         $appsecret= "6a5574a26d9bc3db5a3df198f16d855d";
         $code = $request->input('code');
         $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$appsecret&code=$code&grant_type=authorization_code";
-        exit;
         $token_json = file_get_contents($url);
         $token_arr = json_decode($token_json,true);
         print_r($token_arr);
