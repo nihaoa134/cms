346e2e506896338196a18f55498a57e259d9cc6f
nihaoa134 <2337865364@qq.com>
2019-03-23 08:29:33 +0000

    111

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -168,7 +168,7 @@ class WxController extends Controller
              $arr = $redis -> hGetAll($v);
              array_push($res,$arr);
          }
-        print_r($arr);
+        print_r($data);
 	 }
 
 }
