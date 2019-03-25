2234c7ceb00408dfdedd15dcee456ddd63e23ff2
nihaoa134 <2337865364@qq.com>
2019-03-25 05:37:04 +0000

    1

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -16,7 +16,7 @@ class WxController extends Controller
      */
     public function validToken1()
     {
-//        echo $_GET['echostr'];
+        echo $_GET['echostr'];
         $data = file_get_contents("php://input");
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
