b1a6b2768a278133593ccb14c2d4d5460d134140
nihaoa134 <2337865364@qq.com>
2019-03-30 01:26:02 +0000

    1

--- resources/views/user/kefu.blade.php
+++ resources/views/user/kefu.blade.php
@@ -4,81 +4,83 @@
 <head>
     <script type="text/javascript" src="../js/jquery.min.js"></script>
     <script  src="../js/jquery-1.7.2.min.js"></script>
-    <meta charset="UTF-8">
-    <title>HTML5模拟微信聊天界面</title>
-    <style>
-        /**重置标签默认样式*/
-        * {
-            margin: 0;
-            padding: 0;
-            list-style: none;
-            font-family: '微软雅黑'
-        }
-        #container {
-            height: auto;
-            background: #eee;
-            position: relative;
-        }
-        .header {
-            background: #000;
-            height: 40px;
-            color: #fff;
-            line-height: 34px;
-            font-size: 20px;
-            padding: 0 10px;
-        }
-        .footer {
-            width: 1683px;
-            height: 50px;
-            background: #666;
-            position: absolute;
-            bottom: 0;
-            padding: 10px;
-        }
-        .footer input {
-            width: 1500px;
-            height: 45px;
-            outline: none;
-            font-size: 20px;
-            text-indent: 10px;
-            border-radius: 6px;
-            right: 80px;
-        }
-        .footer span {
-            display: inline-block;
-            width: 162px;
-            height: 48px;
-            background: #ccc;
-            font-weight: 900;
-            line-height: 45px;
-            cursor: pointer;
-            text-align: center;
-            position: absolute;
-            right: 10px;
-            border-radius: 6px;
-        }
-        .footer span:hover {
-            color: #fff;
-            background: #999;
-        }
-        #user_face_icon {
-            display: inline-block;
-            background: red;
-            width: 60px;
-            height: 60px;
-            border-radius: 30px;
-            position: absolute;
-            bottom: 6px;
-            left: 14px;
-            cursor: pointer;
-            overflow: hidden;
-        }
-        img {
-            width: 60px;
-            height: 60px;
-        }
+        <meta charset="UTF-8">
+        <title>HTML5模拟微信聊天界面</title>
+        <style>
+            /**重置标签默认样式*/
+            * {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+                font-family: '微软雅黑'
+            }
+            #container {
+                height: 780px;
+                background: #eee;
+                position: relative;
+                box-shadow: 20px 20px 55px #777;
+            }
+            .header {
+                background: #000;
+                height: 40px;
+                color: #fff;
+                line-height: 34px;
+                font-size: 20px;
+                padding: 0 10px;
+            }
+            .footer {
+                width: 1680px;
+                height: 50px;
+                background: #666;
+                position: absolute;
+                bottom: 0;
+                padding: 10px;
+            }
+            .footer input {
+                width: 1500px;
+                height: 45px;
+                outline: none;
+                font-size: 20px;
+                text-indent: 10px;
+                position: absolute;
+                border-radius: 6px;
+            }
+            .footer span {
+                display: inline-block;
+                width: 150px;
+                height: 48px;
+                background: #ccc;
+                font-weight: 900;
+                line-height: 45px;
+                cursor: pointer;
+                text-align: center;
+                position: absolute;
+                right: 10px;
+                border-radius: 6px;
+            }
+            .footer span:hover {
+                color: #fff;
+                background: #999;
+            }
+            #user_face_icon {
+                display: inline-block;
+                background: red;
+                width: 60px;
+                height: 60px;
+                border-radius: 30px;
+                position: absolute;
+                bottom: 6px;
+                left: 14px;
+                cursor: pointer;
+                overflow: hidden;
+            }
+            img {
+                width: 60px;
+                height: 60px;
+            }
         .content {
             font-size: 20px;
+            width: 435px;
             height: 662px;
             overflow: auto;
             padding: 5px;
